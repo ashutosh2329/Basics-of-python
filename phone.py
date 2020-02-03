b4aab967bd4ab97f23b352d65056a1dc2bd0d097
ashutosh <sonutiwari2329@gmail.com>
2020-02-03 19:38:40 +0530

    Add files via upload

---
+++ phone.py
@@ -0,0 +1,20 @@
+n=input("phone:")
+numbers={
+    "1" : "One ",
+    "2" :  "Two ",
+    "3" : "Three ",
+    "4" : "Four ",
+    "5" : "Five ",
+    "6" : "Six ",
+    "7" : "Seven ",
+   "8" : "Eight ",
+    "9" : "Nine "
+}
+output = ""
+for h in n:
+    output +=numbers.get(h,"!")
+
+print(output)
+
+
+
